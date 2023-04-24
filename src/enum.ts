1a0a0f8d46969fc257f88c6ad971e93d1706d899
Felipe Alvarenga <felipe.alvarenga01@yahoo.com.br>
2023-04-24 11:48:56 -0300

    insercao de erro de email

--- src/enum.ts
+++ src/enum.ts
@@ -3,5 +3,5 @@ export enum FirebaseResponseMessage {
     WRONG_PASSWORD = 'senha inválida.',
     INVALID_EMAIL = 'Email inválido.',
     MISSING_PASSWORD = 'Preencha sua senha.',
-    EMAIL_NOT_FOUND = 'Email inválido.',
+    EMAIL_NOT_FOUND = 'Email não encontrado.',
 }
